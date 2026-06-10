d5395fb8c647abffb49144db0598031f69b71a66
Oluwatobiloba <oluwateezzy03@gmail.com>
2026-06-10 12:21:59 +0100

    refactor: update script source and implement async demonstration with timers in index.js

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
 let age = "tobi"
+
 let name = "tobi"
+
 let isValid = true
 let x = null
 let y = undefined
 
-console.log(typeof age)
-console.log(typeof name)
-console.log(typeof isValid)
-console.log(typeof x)
-console.log(typeof y)
+// console.log(typeof age)
+// console.log(typeof name)
+// console.log(typeof isValid)
+// console.log(typeof x)
+// console.log(typeof y)
 
-if (typeof age === "number"){
-    console.log(age)
-} else {
-    console.log("Please input the right value")
+// console.log("Waiting !!!")
+// let xx = setTimeout(() => {
+//     console.log("Waiting over !!!")
+//     // fetch all products
+//     // return all products
+// }, 30000)
+
+// console.log(xx)
+
+// Promise.all([
+//     setTimeout((resolve, error) => {
+//         console.log("Waiting over !!!")
+//         // fetch all products
+//         // return all products
+//     }, 30000),
+//     setTimeout(() => {
+//         console.log("Waiting over !!!")
+//         // fetch all products
+//         // return all products
+//     }, 40000)
+// ])
+
+// console.log("I'm waiting")
+
+// if (typeof age === "number"){
+//     console.log(age)
+// } else {
+//     console.log("Please input the right value")
+// }
+
+async function doSomething() {
+    await setTimeout(() => {
+        console.log("Waiting over !!!")
+    }, 40000)
+
+    await setTimeout(() => {
+        console.log("Waiting over !!!")
+    }, 40000)
+
+    await setTimeout(() => {
+        console.log("Waiting over !!!")
+    }, 40000)
+
+    await setTimeout(() => {
+        console.log("Waiting over !!!")
+    }, 40000)
+
+    await setTimeout(() => {
+        console.log("Waiting over !!!")
+    }, 40000)
+
+    return "oop!"
 }
+
+doSomething()
+
+console.log(
+    "Ohh I'm waiting for doSomething function"
+)
